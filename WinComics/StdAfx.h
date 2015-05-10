95007dba682883615d106b9a63012b633b17f752
Evan Reynolds <evan@evan.org>
2015-05-09 22:04:06 -0700

    Working on porting to Studio 2013

--- WinComics/StdAfx.h
+++ WinComics/StdAfx.h
@@ -12,10 +12,10 @@
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
-#define WINVER         0x0400
-#define _WIN32_WINNT   0x0400
-#define _WIN32_WINDOWS 0x0400
-#define _WIN32_IE      0x0400
+#define WINVER         0x0501
+#define _WIN32_WINNT   0x0501
+#define _WIN32_WINDOWS 0x0501
+#define _WIN32_IE      0x0501
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
